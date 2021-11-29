4e65564ef521d149761acad515cee3c6ca6aad57
Neylor <nwsbagagi@gmail.com>
2021-11-29 18:00:35 -0300

    APIClient test

--- ConcreteChallengeTests/APIClientTests.swift
+++ ConcreteChallengeTests/APIClientTests.swift
@@ -18,9 +18,81 @@ class APIClientTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testGetMovies() throws {
+        // ARRANGE
+        let page = 1
+
+        // ACT
+        var data: [Movie] = []
+        var error: Error?
+
+        let expectation = self.expectation(description: "Load API Data")
+        APIClient.share.getMovies(forPage: String(page)) { (result) in
+            switch result {
+            case .failure(let resultError):
+                error = resultError
+            case .success(let resultData):
+                data = resultData
+            }
+            expectation.fulfill()
+        }
+
+        // ASSERT
+        waitForExpectations(timeout: 5.0, handler: nil)
+        XCTAssertNil(error, "result must not fail \(error.debugDescription)")
+        XCTAssertFalse(data.isEmpty, "result data must not be empty")
+    }
+
+    func testGetMoviesIncrement() throws {
+        // ARRANGE
+        let pages = 2
+
+        // ACT
+        var totalData: [Movie] = []
+        let expectation = self.expectation(description: "Load API Data")
+        expectation.expectedFulfillmentCount = 2
+
+        for page in 1...pages {
+            APIClient.share.getMovies(forPage: String(page)) { (result) in
+                switch result {
+                case .failure(let error):
+                    XCTFail(error.localizedDescription)
+                case .success(let data):
+                    totalData.append(contentsOf: data)
+                }
+                expectation.fulfill()
+            }
+        }
+
+        // ASSERT
+        waitForExpectations(timeout: 15.0, handler: nil)
+        XCTAssertFalse(totalData.isEmpty)
+        XCTAssertTrue(totalData.count == 40, "totalData must to contain 40 objects on second iteration")
+
+    }
+
+    func testGetGenres() throws {
+        // ARRANGE
+
+        // ACT
+        var data: [Genre] = []
+        var error: Error?
+
+        let expectation = self.expectation(description: "Load API Data")
+        APIClient.share.getGenres { (result) in
+            switch result {
+            case .failure(let resultError):
+                error = resultError
+            case .success(let resultData):
+                data = resultData
+            }
+            expectation.fulfill()
+        }
+
+        // ASSERT
+        waitForExpectations(timeout: 5.0, handler: nil)
+        XCTAssertNil(error, "result must not fail \(error.debugDescription)")
+        XCTAssertFalse(data.isEmpty, "result data must not be empty")
     }
 
     func testPerformanceExample() throws {
